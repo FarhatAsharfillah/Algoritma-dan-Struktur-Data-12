70c521880a1d962148738fb02b84910c31c028b1
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2024-05-31 14:46:08 +0700

    commit 7 input void remove dengen if untuk mengecek apakah antrian kosong

--- circularqueues.cpp
+++ circularqueues.cpp
@@ -36,5 +36,15 @@ void insert() {
             REAR =  REAR + 1;
         
     }
+    queues_array[REAR] = num;
+}
+
+void remove() {
+    //cek apakah antrian kosong
+    if (FRONT == -1) {
+        cout << "queue underflow\n";
+        return;
+    }
+    cout <<"\nThe element deleted from the queue is: " << queues_array[FRONT] << "\n"
 }
 }
