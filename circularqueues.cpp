f442abcb162269291f3fa7a7300a33b329ab9cd5
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2024-05-31 14:38:44 +0700

    commit 5 input if untuk mengecek apakah antrian kosong

--- circularqueues.cpp
+++ circularqueues.cpp
@@ -22,5 +22,11 @@ void insert() {
         cout << "\nQueue overflow\n";
         return;
     }
+
+    //cek apakah antrian kosong
+    if (FRONT == -1) {
+        FRONT = 0;
+        REAR = 0;
+    }
 }
 }
