d3fb8b1ef92f02a86c5e589aecc29af8ac06a488
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2024-05-31 14:52:23 +0700

    commit 9 input else untuk kondisi jika elemen yang dihapus berada di posisi terakhir

--- circularqueues.cpp
+++ circularqueues.cpp
@@ -51,6 +51,14 @@ void remove() {
     if (FRONT == REAR) {
         FRONT = -1;
         FRONT = -1;
+        REAR = -1;
+    }
+    else {
+        //jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
+        if(FRONT == max - 1)
+        FRONT = 0;
+        else
+        FRONT = FRONT + 1;
     }
 
 }
